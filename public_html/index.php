c4a2c65aa8a627d30c29a25dd34dfe383eb88824
Saeid Zebardast <saeid@datispars.com>
2015-06-06 08:48:16 +0430

    - Add basic favicon and index.php;

---
+++ public_html/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8">
+<meta name="keywords" content="Text, Editor, ویرایشگر متن" />
+    <meta name="description" content="ویرایشگر ساده و سریع متن، Quick text editor"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="shortcut icon" type="image/png" href="./favicon.png"/>
+    <link rel="canonical" href="http://textarea.ir" />
+    <meta name="author" content="Saeid Zebardast"/>
+    <meta property="og:title" content="ویرایشگر سریع و ساده متن Textarea"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="http://textarea.ir/favicon.png"/>
+    <meta property="og:url" content="http://textarea.ir"/>
+    <meta property="og:description" content="ویرایشگر سریع و ساده متن Textarea"/>
+    <meta name="twitter:card" content="ویرایشگر سریع و ساده متن Textarea">
+    <meta name="twitter:url" content="http://textarea.ir">
+    <meta name="twitter:title" content="ویرایشگر سریع و ساده متن Textarea">
+    <meta name="twitter:description" content="ویرایشگر سریع و ساده متن Textarea">
+    <meta name="twitter:image" content="http://textarea.ir/favicon.png">
+    <title>ویرایشگر سریع و ساده متن Textarea</title>
+</head>
+<body>
+<h1 style="direction: rtl; text-align: right;">
+بزودی...
+</h1>
+<textarea placeholder="بزودی ..." style="width: 100%; height: 250px; border: 1px solid #333; direction: rtl; text-align: right">
+</textarea>
+</body>
+</html>
