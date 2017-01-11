f8d7c2633b49bf2178434e7eaacd9a0572d025b4
Saeid Zebardast <saeid.zebardast@gmail.com>
2017-01-11 15:33:22 +0330

    Fix #1 (Save text in localStorage)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,9 @@
 /**
-* http://code.runnable.com/U5HC9xtufQpsu5aj/use-javascript-to-save-textarea-as-a-txt-file
-*/
-function saveTextAsFile(textAreaId) {
+ * http://code.runnable.com/U5HC9xtufQpsu5aj/use-javascript-to-save-textarea-as-a-txt-file
+ */
+function saveTextAsFile(textareaId) {
   // grab the content of the form field and place it into a variable
-  var textToWrite = document.getElementById(textAreaId).value;
-  textToWrite = addNewLines(textToWrite);
+  var textToWrite = getTextContent(textareaId);
   //  create a new Blob (html5 magic) that conatins the data from your form feild
   var textFileAsBlob = new Blob([textToWrite], {
     type: 'text/plain'
@@ -44,8 +43,11 @@ function saveTextAsFile(textAreaId) {
   downloadLink.click();
 }
 
+/**
+ * remove the link from the DOM
+ * @param event
+ */
 function destroyClickedElement(event) {
-  // remove the link from the DOM
   document.body.removeChild(event.target);
 }
 
@@ -59,3 +61,44 @@ function addNewLines(text) {
   text = text || '';
   return text.replace(/\n/g, "\r\n");
 }
+
+function getTextContent(textareaId) {
+  var text = document.getElementById(textareaId).value;
+  text = addNewLines(text);
+  return text;
+}
+
+/**
+ * Save content of textarea to localStorage
+ * @param textareaId
+ */
+function saveTextInLocalStorage(textareaId) {
+  localStorage.setItem('text', getTextContent(textareaId))
+}
+
+/**
+ * Load text from localStorage and update textarea content.
+ * @param textareaId
+ */
+function loadPreviousText(textareaId) {
+  document.querySelector('#' + textareaId).value = localStorage.getItem('text');
+}
+
+/**
+ * Clear text and localStorage data.
+ */
+function clearText(textareaId) {
+  document.querySelector('#' + textareaId).value = '';
+  localStorage.removeItem('text');
+
+}
+
+window.onload = function () {
+  var textareaId = document.querySelector('textarea').id;
+
+  loadPreviousText(textareaId);
+
+  window.addEventListener('input', function () {
+    saveTextInLocalStorage(textareaId)
+  });
+};
